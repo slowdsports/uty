9612648a8b8cbb5d08ae535466d128f01e71f395
slowdsports <72911530+slowdsports@users.noreply.github.com>
2024-11-07 10:44:41 -0600

    Update server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
   originBlacklist: originBlacklist,
-  originWhitelist: ['https://futbolhonduras24.com', 'https://megadeportestv2.online', 'https://st.nowomi.quest', 'https://nowomi.quest'],
+  originWhitelist: ['https://futbolhonduras24.com', 'https://megadeportestv2.online', 'https://crimson-dela-26.tiiny.io/', 'https://st.nowomi.quest', 'https://nowomi.quest'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
